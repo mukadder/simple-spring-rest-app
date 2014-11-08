157e5817d10b3d478c2e6bac63cb33ed84bda43e
Andrew DeMaria <lostonamountain@gmail.com>
2014-11-08 15:11:46 -0600

    Deleted old pastebin application

--- src/main/java/simpleapp/Application.java
+++ src/main/java/simpleapp/Application.java
@@ -5,20 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableAutoConfiguration(exclude = JmsAutoConfiguration.class)
 @ComponentScan
-@EnableAutoConfiguration(exclude = {JmsAutoConfiguration.class})
-@EnableJpaRepositories
-@Import(RepositoryRestMvcConfiguration.class)
-@ImportResource("activemq.xml")
+@EnableWebMvc
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
